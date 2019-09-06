945294bf1e3834b6f532d5d6caaee61d21c339ec
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-09-06 15:46:26 -0600

    Tried to understand some basics
    I was trying to understand what a variable was, so followed unity definition by making a game object and made it's name a different name. don't know what it does, don't know it's purpose but it worked.

--- CodeBlue/Assets/ghostscript.cs
+++ CodeBlue/Assets/ghostscript.cs
@@ -1,27 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Ghostscript : MonoBehaviour
+public class GhostScript : MonoBehaviour
 {
-	public static int Grim = 100;
-
-	// Use this for initialization
+	public GameObject grimm;
 	void Start()
 	{
-	//figure out what this is and how to make it work''
-		object foo;
-		var i = Grim;
-		int i1 = Grim;
-		Grim = float = i;
-		
-		//some console text
-		UnityEngine.MonoBehaviour.print("Let's get started.");
-	}
-//this has something to do with the Grim text I started but IDK what
-	public static int Grim1
-	{
-		get { return Grim; }
-		set { Grim = value; }
+		grimm.name = "Reaper";
 	}
 }
