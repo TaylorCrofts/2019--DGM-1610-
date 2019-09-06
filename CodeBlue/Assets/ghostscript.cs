d664a71fb7d9950b9bbfad3597aa0d95a5d5a6e1
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-09-06 10:35:48 -0600

    Tried to code, lots of mistakes that I don't know how to solve yet

---
+++ CodeBlue/Assets/ghostscript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghostscript : MonoBehaviour
+{
+	public static int Grim = 100;
+
+	// Use this for initialization
+	void Start()
+	{
+	//figure out what this is and how to make it work''
+		object foo;
+		var i = Grim;
+		int i1 = Grim;
+		Grim = float = i;
+		
+		//some console text
+		UnityEngine.MonoBehaviour.print("Let's get started.");
+	}
+//this has something to do with the Grim text I started but IDK what
+	public static int Grim1
+	{
+		get { return Grim; }
+		set { Grim = value; }
+	}
+}
