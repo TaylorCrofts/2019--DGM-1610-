f37e5924203d8d4f6e51475bc92388ebb09c604a
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-09-08 19:33:00 -0600

    Trying to make it move and failing

--- CodeBlue/Assets/ghostscript.cs
+++ CodeBlue/Assets/ghostscript.cs
@@ -1,12 +1,30 @@
-﻿using UnityEditor.VersionControl;
+﻿using System;
+using UnityEditor.Animations;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.Networking;
 
 public class GhostScript : MonoBehaviour
 {
-	public GameObject grimm;
+	public Motion grimm;
+	private Motion _motion;
+
 	void Start()
 	{
-		grimm.name = "Reaper";
+//		//{ changed the name of object by assigning a new varible and I think I'm giving it motion. IDK it's something I found and I hope it has something to do with moving the object with my computer keys
+//			_motion = grimm;
+//			grimm.name = "Reaper";
+//		}
 		print("Hello?");
 	}
-}
+
+//Don't know how to make object move with keys'
+//	public void Update()
+//	{
+//		if (grimm).KeyDownEvent(KeyCode.w);
+//		{
+//			GetComponent<ChildMotion>(100).position.Equal(Invoke(float.02);
+//		}
+//		throw new NotImplementedException();
+	}
