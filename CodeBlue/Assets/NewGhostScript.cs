a4430986a6a7ff3e469e57158657e57e8e61eb46
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-09-09 22:55:55 -0600

    Made the particles follow and circle disappear when collided with

--- CodeBlue/Assets/NewGhostScript.cs
+++ CodeBlue/Assets/NewGhostScript.cs
@@ -9,17 +9,22 @@ public class NewGhostScript : MonoBehaviour
 {
 	public float floatValue = 5.5f;
 	public int intValue = 20;
-	public string stringValue = "Reaper";
+	public string stringValue = "Ghost";
 	public UnityEvent Event;
+	public int firepower = 1;
 
 	private void OnTriggerEnter(Collider other)
 	{
 		Event.Invoke();
 		print("Hello Spirit");
 	}
-
-	private void Start()
-	{
-		
-	}
 }
+
+//Need to find and figure out what oftype is
+//I want the Circle to become the player and the square to "disappear" until (X) is pressed on the keyboard
+//I then want the circle to freeze where it's at and have the square reappear.
+	//private void OnCollisionExit(Collider other)
+	//{
+	//	Event.Invoke();
+	//	print("Jiggly");
+	//}
