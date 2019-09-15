6aedca54f3e287fde6bf92f43847f58194995ff9
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-09-14 21:09:13 -0600

    NoteScript started

--- Paradox/Assets/NoteScript.cs
+++ Paradox/Assets/NoteScript.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//public=Everyone/Unity can see it/ Class=Class(cookie cutter)
+//"NoteScript"=the name you gave the script that says what it do.
+//(:=instantiate) MonoBehaviour-is a component that can be added to GameObjects
+//{}=scripting home) (scripts are introverts they must have their currlyBrakets!
 public class NoteScript : MonoBehaviour {
 
-	// Use this for initialization
+	// Use this for initialization. (Void=function) Start=happens play is pressed
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frame. Update is constantly happening(blablabla...)
 	void Update () {
 		
 	}
