eb4d48fbfdd87b8366de57dd1b11554579f392b3
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-09-14 20:32:04 -0600

    The Paradox Project At Home script

---
+++ Paradox/Assets/HealthFloatScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class HealthFloatData : ScriptableObject
+{
+    public float value = 5f;
+
+    public void UpdateHealthValue(float amount)
+    {
+        value += amount;
+    }
+}
---
+++ Paradox/Assets/TriggerEventScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventScript : MonoBehaviour 
+{
+    public UnityEvent triggerEvent;
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEvent.Invoke();
+        print("I am colliding");
+    }
+}
