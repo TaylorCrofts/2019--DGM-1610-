161f9c521407f02f960247113abc28c1baa8027d
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-09-15 22:34:27 -0600

    ParticlesOnMouseDown particles.Emit

--- Paradox/Assets/ParticleEvents.cs
+++ Paradox/Assets/ParticleEvents.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class ParticleEvents : MonoBehaviour
 {
-
-
+    public ParticleSystem particles;
+    private void OnMouseDown()
+    { 
+        particles.Emit(420);
+    }
 }
+
