4793b310ddb3345dc7cbc8460f41b9bd833e2c27
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-09-17 23:06:46 -0600

    1st Practicing with Charactor controller
    Give the gravity a negative and adjust the jump

---
+++ Paradox/Assets/Movement.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+using UnityEngine.Experimental.UIElements.StyleEnums;
+
+public class Movement : MonoBehaviour
+{
+	public float speed = 5f;
+	public float gravity = 5f;
+	public float jumpSpeed = 10f;
+
+	private Vector3 position;
+	public CharacterController controller;
+	void Update ()
+	{
+		position.x = speed * Input.GetAxis("Horizontal");
+		if (!controller.isGrounded)
+		{
+			position.y +=gravity;
+		}
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			position.y = jumpSpeed;
+		}
+
+		controller.Move(position * Time.deltaTime);
+	}
+}
