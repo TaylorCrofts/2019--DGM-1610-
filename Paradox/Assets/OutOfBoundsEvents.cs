4918428bc9411ac7926d6c4d3f4f6b518c39a848
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-09-25 23:43:59 -0600

    Trying to make a out of bounds floor

---
+++ Paradox/Assets/OutOfBoundsEvents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OutOfBoundsEvents : MonoBehaviour
+{
+	public UnityEvent CollisionEvent;
+	public void OnTriggerEnter(Collider other)
+	{
+		print("Death");
+	}
+}
