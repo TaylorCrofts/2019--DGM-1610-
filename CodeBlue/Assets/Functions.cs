ecfd6ad6a28114e1b4d40842231e917c2d7ec271
Taylor <jocelyncrofts@gmail.com>
2019-09-26 15:45:18 -0600

    More of the ReturnFunction

--- CodeBlue/Assets/Functions.cs
+++ CodeBlue/Assets/Functions.cs
@@ -8,27 +8,31 @@ using UnityEngine;
 //use them when you know you want the same option to be repeated several times.
 //Unity Events must have a return type of void. 
 //(instansiate) means to create.
-
+//build stuff and turn it into a prefab
+//If were declaring a float we must return a float ect...
 public class Functions : MonoBehaviour
 {
+	public Color playercolor= Color.red;
+	
 	public string playerName;
 	private int myNumber;
 	public float Speed = 5;
 	public GameObject player;
 	void Start ()
 	{
-		Instantiate(configGameObject());
+		Instantiate(ConfigGameObject());
 	}
 
-	private GameObject configGameObject()
+	private GameObject ConfigGameObject()
 	{
-		player.layer = 0;
-		player.active = true;
-		player.tag = "Player";
-		player.transform.position=Vector3.zero;
-		player.name = playerName;
+		var Newplayer = Instantiate(player);
+		Newplayer.layer = 0;
+		Newplayer.active = true;
+		Newplayer.tag = "Player";
+		Newplayer.transform.position=Vector3.zero;
+		Newplayer.name = playerName;
 		
-		return gameObject;  
+		return Newplayer;  
 	}
 	
 	private float IncreaseSpeed(float multiplier)
