ed9028000d38fe432a9e5820c450caea63da4157
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-10-05 21:55:13 -0600

    Added Jumpcounts to my movement script

--- Paradox/Assets/movementScripts/Movement.cs
+++ Paradox/Assets/movementScripts/Movement.cs
@@ -1,28 +1,36 @@
-﻿
-using UnityEngine;
-using UnityEngine.Experimental.UIElements.StyleEnums;
-
+﻿using UnityEngine;
 public class Movement : MonoBehaviour
 {
 	public float speed = 5f;
 	public float gravity = 5f;
 	public float jumpSpeed = 10f;
 
+	private int jumpcount = 1;
+	
 	private Vector3 position;
+	
 	public CharacterController controller;
 	void Update ()
 	{
-		position.x = speed * Input.GetAxis("Horizontal");
+		position.x = speed*Input.GetAxis("Horizontal");
+
 		if (!controller.isGrounded)
 		{
-			position.y +=gravity;
+			position.y += gravity;
 		}
 
-		if (Input.GetButtonDown("Jump"))
+		else
+		{
+			jumpcount = 1;
+		}
+		
+		if (Input.GetButtonDown("Jump") && jumpcount > 0)
 		{
 			position.y = jumpSpeed;
+			jumpcount--;
 		}
 
 		controller.Move(position * Time.deltaTime);
 	}
 }
+//The public (Float)'s are given a name and assigned a value which the program can understand. 
