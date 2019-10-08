2493cf0c45db3dd2754264f47fa1041fcba1bfef
Taylor <jocelyncrofts@gmail.com>
2019-10-08 15:53:35 -0600

    Todays lesson
    Inhearatance, scriptable objects

---
+++ Paradox/Assets/Players/PlayerData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class PlayerData : GameArtData
+{
+	public WeaponData weapon;
+	
+	public void instancePlayer()
+	{
+		var newPlayer=Instantiate(prefab);
+		var newSprite = newPlayer.GetComponentInChildren<SpriteRenderer>();
+		newSprite.color = color;
+		newSprite.sprite = sprite;
+	}
+
+}
