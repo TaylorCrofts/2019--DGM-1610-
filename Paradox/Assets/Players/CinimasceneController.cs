8b08d85b54e97dd265ac72dabdf57249ec5dde2d
Taylor <jocelyncrofts@gmail.com>
2019-10-15 15:54:10 -0600

    UnityActions

---
+++ Paradox/Assets/Players/CinimasceneController.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CinimasceneController : MonoBehaviour
+{
+	public PlayerData playerdata;
+private CinemachineVirtualCamera virtualCamera;
+	void Start ()
+	{
+		virtualCamera = GetComponent<CinemachineVirtualCamera>();
+		playerdata.instanceAction = instanceHandler;
+			playerdata.InstancePlayer();
+	}
+
+	private void instanceHandler(GameObject obj)
+	{
+		virtualCamera.Follow = obj.transform;
+	}
+}
