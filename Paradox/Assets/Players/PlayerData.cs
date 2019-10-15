b1013c2e327cf5926ddbe0d16b2eeaa005240203
Taylor <jocelyncrofts@gmail.com>
2019-10-15 15:27:55 -0600

    Playerdata

--- Paradox/Assets/Players/PlayerData.cs
+++ Paradox/Assets/Players/PlayerData.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 [CreateAssetMenu]
 public class PlayerData : GameArtData
 {
+	public UnityAction<GameObject> instanceAction;
+	
 	public List<WeaponData> weapon;
 	public FloatData health;
 	public Clothesdata shirt;
 	public Clothesdata pants;
 	
-	public void instancePlayer()
+	public void InstancePlayer()
 	{
 		var newPlayer=Instantiate(prefab);
 		var newSprite = newPlayer.GetComponentInChildren<SpriteRenderer>();
 		newSprite.color = color;
 		newSprite.sprite = sprite;
+		instanceAction(newPlayer);
 	}
 
 }
