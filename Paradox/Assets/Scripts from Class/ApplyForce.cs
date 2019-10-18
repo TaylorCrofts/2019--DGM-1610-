020aa4ec6ad6757cc5f3eb5d1229dbf1fb820000
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-10-17 21:14:08 -0600

    Added force script, and installed Cinemachine to my computer

---
+++ Paradox/Assets/Scripts from Class/ApplyForce.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+
+public class ApplyForce : MonoBehaviour 
+{
+    private Rigidbody rigBod;
+    public Vector3 forces;
+    
+    void Start()
+    {
+        rigBod = GetComponent<Rigidbody>();
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        rigBod.AddForce(forces);    
+    }
+}
