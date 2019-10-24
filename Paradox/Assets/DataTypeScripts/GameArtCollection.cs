2b178970bf871ec232418732114d98a8e578ea42
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-10-24 00:05:37 -0600

    Added art collection Script Watched Brackies

---
+++ Paradox/Assets/DataTypeScripts/GameArtCollection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class GameArtCollection : ScriptableObject
+{
+	public List<GameArtData> artCollection;
+	public void AddArtData(GameArtData obj)
+	{
+		artCollection.Add(obj);
+		obj.collected = true;
+	}
+}
--- Paradox/Assets/DataTypeScripts/GameArtData.cs
+++ Paradox/Assets/DataTypeScripts/GameArtData.cs
@@ -6,4 +6,5 @@ public class GameArtData : ScriptableObject
 		public Sprite sprite;
         	public Color color= Color.red;
         	public GameObject prefab;
+            public bool collected;
 }
