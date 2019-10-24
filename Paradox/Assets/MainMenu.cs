bc011fe1ef0c04d0cc91b49520616682bf93759a
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-10-23 23:32:55 -0600

    The Exit button Works

--- Paradox/Assets/MainMenu.cs
+++ Paradox/Assets/MainMenu.cs
@@ -7,4 +7,10 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 
+    public void ExitGame()
+    {
+        print("You've left the game");
+        Application.Quit();
+    }
+
 }
