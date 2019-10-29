dbb9f1f6418470ff5dbce1b84d06d20f36507739
Taylor <jocelyncrofts@gmail.com>
2019-10-29 15:41:08 -0600

    SwitchingKrakUnityPro

---
+++ CodeBlue/Assets/ScriptsFromClass/DeBugger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class DeBugger : ScriptableObject
+{
+	public void OnDebug (String words)
+	{
+		Debug.Log(words);
+	}
+
+	public void OnDebug()
+	{
+		Debug.Log(name);
+
+	}
+}
