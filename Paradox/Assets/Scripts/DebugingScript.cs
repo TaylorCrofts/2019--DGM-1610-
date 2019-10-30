33c1af4ba86b4a3e2ab4503a75680c1bfb20cabe
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-10-30 16:45:47 -0600

    Adding missing scripts
    Debug,Enum,Functions

---
+++ Paradox/Assets/Scripts/DebugingScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class DebuggingScript : ScriptableObject
+{
+    public void OnDebug(string words)
+    {
+        Debug.Log(words);
+    }
+
+    public void OnDebug()
+    {
+        Debug.Log(name);
+    }
+}
---
+++ Paradox/Assets/Scripts/EnumScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class EnumScript : MonoBehaviour 
+{
+    public enum States
+    {
+        Start,
+        Playing,
+        End
+    }
+
+    public States currentState;
+    public UnityEvent onStartEvent, onPlayingEvent, onEndEvent;
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case States.Start:
+                onStartEvent.Invoke();
+                print("Starting");
+                break;
+            case States.Playing:
+                onPlayingEvent.Invoke();
+                print("Playing");
+                break;
+            case States.End:
+                onEndEvent.Invoke();
+                print("Ending");
+                break;
+            default:
+                onStartEvent.Invoke();
+                print("Default Start");
+                break;
+        }
+    }
+}
