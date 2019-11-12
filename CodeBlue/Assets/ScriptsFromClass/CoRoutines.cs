22b0c879c26b47a05117613761cb0c55cc4f06e1
Taylor <jocelyncrofts@gmail.com>
2019-11-12 15:54:02 -0700

    Saving Class stuff

---
+++ CodeBlue/Assets/ScriptsFromClass/CoRoutines.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoRoutines : MonoBehaviour
+{
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    private WaitForSeconds wfsObj;
+    public int counter = 10;
+    public float seconds = 10f;
+    public bool canRun; //Does the same as counter but infinitely repeats
+    
+
+    private void Awake()
+    {
+        wfsObj= new WaitForSeconds(seconds);
+    }
+
+    public void CallCoRoutine()
+    {
+        StartCoroutine(RunCoroutine());
+    }
+
+    IEnumerator RunCoroutine()
+    {
+        startEvent.Invoke();
+    while (counter > 0)
+    {
+        repeatEvent.Invoke();
+        yield return wfsObj;
+    Debug.Log(counter);
+    counter--;
+    }
+    endEvent.Invoke();
+    yield return wfsObj;
+    Debug.Log("Go!");
+     }
+    
+    
+    
+}
+
+//________________________eh code________________
+   // private int i = 3;
+
+    //IEnumerator Start()
+    //{
+        //while (i > 0)
+        //{
+          //  yield return new WaitForSeconds(1f);
+            //Debug.Log(i);
+            //i--;
+        //}
+
+        //yield return new WaitForSeconds(1f);
+        //Debug.Log("Go!");
+   // }
+
+
+
+
+
+
+
+//__________________BdCode__________
+    //IEnumerator Start()
+    //{
+    // yield return new WaitForSeconds(1f);
+    //Debug.Log(3);
+    //yield return new WaitForSeconds(1f);
+    //Debug.Log(2);
+    //yield return new WaitForSeconds(1f);
+    //Debug.Log(1);
+    //yield return new WaitForSeconds(1f);
+    //Debug.Log("Go!");
+    //}
+    //private void Start()
+    //{
+    // Invoke(nameof("StartLater"), time:3f);
+// }
+
+// void Counter(int number)
+// {
+    //    Debug.log("run")
+// }
+// void StartLater()
+// {
+    // Debug.Log("Ran");
+// }
+
+
