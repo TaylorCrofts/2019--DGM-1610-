dd2e5b6cbe7433eaf3c36c29f3077c50de220760
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-11-15 23:51:50 -0700

    All Caught up for class

---
+++ CodeBlue/Assets/ScriptsFromClass/GameArtCollection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu]
+public class GameArtCollection : ScriptableObject
+{
+    public List<GameArtData> collectionList;
+
+    public void AddData(GameArtData obj)
+    {
+        if (!collectionList.Contains(obj))
+        {
+            collectionList.Add(obj);
+        }
+    }
+
+    public void RemoveLastItem()
+    {
+        if (collectionList.Capacity != 0)
+        {
+            collectionList.RemoveAt(collectionList.Count-1);
+        }
+    }
+}
---
+++ CodeBlue/Assets/ScriptsFromClass/Lists.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScriptsFromClass
+{
+    [CreateAssetMenu]
+    public class Lists : ScriptableObject
+    {
+        public List<string> stringList;
+        public List<int> intList;
+        public List<float> floatList;
+
+        public List<GameObject> gameObjectList;
+        public List<FloatData> floatDataList;
+    }
+}
