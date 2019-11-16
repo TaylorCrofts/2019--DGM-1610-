dd5039e90e98ee3fff1e77fe90bb73c34aa45e8d
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-11-15 23:09:53 -0700

    Adding missing scripts

---
+++ CodeBlue/Assets/UIImageController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+[RequireComponent(typeof(Image))]
+public class UIImageController : MonoBehaviour {
+
+    private Image iMAGES;
+    void Awake()
+    {
+        iMAGES = GetComponent<Image>();
+    }
+
+    public void UpdateImage(HealthFloatScript data)
+    {
+        iMAGES.fillAmount = data.value;
+    }
+}
