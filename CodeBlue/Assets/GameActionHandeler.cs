c197f7ae113bf533674f0a2f7028354f340c352a
Taylor <jocelyncrofts@gmail.com>
2019-11-21 15:53:01 -0700

    class animators

---
+++ CodeBlue/Assets/GameActionHandeler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionHandeler : MonoBehaviour
+{
+	public GameAction gameActionObjectOBJ;
+	public UnityEvent handlerEvent;
+
+	void Start ()
+	{
+	//	gameActionObjectOBJ=RaiseHandler;
+	}
+	
+	// void GameAction void RaiseHandler()
+	//{
+		//handlerEvent.Invoke();
+	//}
+
+}
