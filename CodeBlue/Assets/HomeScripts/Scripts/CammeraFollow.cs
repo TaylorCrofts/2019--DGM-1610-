fae964b7a8fefee6068842cb2922f65c9cf45b5e
Taylor J. Crofts <jocelyncrofts@gmail.com>
2019-12-04 16:54:43 -0700

    Camera follow

---
+++ CodeBlue/Assets/HomeScripts/Scripts/CammeraFollow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class CammeraFollow : MonoBehaviour
+{
+	public Transform playerObj;
+	public Vector3 offSet;
+	private void FixedUpdate()
+	{
+		transform.position = playerObj.position + offSet;
+	}
+}
