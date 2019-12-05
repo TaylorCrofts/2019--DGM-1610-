6e95ad2c532e937f21a82dd317626cccb0674035
Taylor <jocelyncrofts@gmail.com>
2019-12-05 15:53:43 -0700

    class workpt2

---
+++ CodeBlue/Assets/ScriptsFromClass/Instancer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Instancer : MonoBehaviour
+{
+    
+}
+//public ApplyForce ammo;
+
+    // Update is called once per frame
+  //  public void Fire()
+  //  {
+   //         Instantiate(ammo,transform.position, Quaternion.identity);
+  //  }
+//}
